35281337258de0e883b906a511015652ba462569
Jimena Hernández <her21199@uvg.edu.gt>
2024-01-29 09:06:13 -0600

    updates

--- components/LogoClouds/LogoClouds.jsx
+++ components/LogoClouds/LogoClouds.jsx
@@ -7,9 +7,9 @@ const LogoClouds = ({logos}) => {
 
     return (
       <div className="py-10">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="marquee-container">
-          <div className="mx-auto flex items-center gap-x-20  marquee h-20">
+        <div className="mx-auto px-6 lg:px-8 ">
+          <div className="marquee-container overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+          <div className="mx-auto flex items-center gap-x-20  marquee h-20 ">
             {logosDuplicados.map((logo) => (
                 <img
                     key={logo}
