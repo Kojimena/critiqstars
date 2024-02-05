f6c06efb823ad1455d14777a03c5252fc6abfa35
Jimena Hernández <her21199@uvg.edu.gt>
2024-02-05 12:04:43 -0600

    Update Timeline.jsx

--- components/Timeline/Timeline.jsx
+++ components/Timeline/Timeline.jsx
@@ -4,7 +4,7 @@ const Timeline = () => {
   return (
     <ul className="timeline xs:timeline-vertical">
   <li>
-    <div className="timeline-start purpleshadow p-4 max-w-48 text-white lg:max-w-80"> Hacemos un análisis de tus cuentas activas en internet. No solo analizamos tu perfil de Google si no tambien otras cuentas generadoras de calificaciones como: Yelp, Facebook, TripAdvisor </div>
+    <div className="timeline-start purpleshadow p-4 max-w-48 text-white lg:max-w-80"> Hacemos un análisis de tus cuentas activas en internet. No solo analizamos tu perfil de Google sino tambien otras cuentas generadoras de calificaciones como: Yelp, Facebook, TripAdvisor </div>
     <div className="timeline-middle">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-purpleultralight"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
     </div>
@@ -36,7 +36,7 @@ const Timeline = () => {
   </li>
   <li>
     <hr className="bg-purpleneon"/>
-    <div className="timeline-start purpleshadow p-4 max-w-40 lg:max-w-80 text-white">Reporte de cierre mensual del perfomance de otras plataformas generadoras de calificaciones</div>
+    <div className="timeline-start purpleshadow p-4 max-w-40 lg:max-w-80 text-white">Reporte de cierre mensual del performance de otras plataformas generadoras de calificaciones</div>
     <div className="timeline-middle">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-purpleultralight"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
     </div>
