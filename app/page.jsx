b5018aaadb20520193e55c7b3c105c6b3a8326d6
Jimena Hernández <her21199@uvg.edu.gt>
2024-03-13 11:57:48 -0600

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -85,7 +85,7 @@ export default function Home() {
         <div className="flex flex-col lg:flex-row lg:items-start items-center justify-start w-full max-h-screen">
             <div className="flex flex-col items-center justify-center lg:w-1/2">
               <Tittle title={"Posiciónate en Google Maps"} color={"#fff"}/>
-              <Subtitle text={"Buenas valoraciones atraerán a más clientes a tu negocio y conseguirás quitarle clientes a tu competencia."} color={"#fff"} />
+              <Subtitle text={"Buenas valoraciones atraerán más clientes a tu negocio de forma orgánica."} color={"#fff"} />
               <div className="flex flex-row items-start justify-start gap-x-4 py-8 w-full h-60">
                 <Stats number={"93%"} description={"de los consumidores escogen negocios con una nota superior a 4 puntos en Google."} />
                 <Stats number={"83.8%"} description={"es la participación de Google en el mercado de motores de búsqueda."} />
